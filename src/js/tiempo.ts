50790acbd9061213fd67a4149855dbbda2504dba
Javier Romero Moreno <jromero@AULAS.TRIANA>
2018-10-24 10:01:55 +0200

    arrow typescript

--- src/js/tiempo.ts
+++ src/js/tiempo.ts
@@ -1,9 +1,8 @@
 var inicio:number=0;
 var crono:any;
 
-
 // Iniciar tiempo
-function iniciarTiempo() {
+/*function iniciarTiempo() {
   if (inicio == 0) {
       let n:number = 1;
       let l:any = document.getElementById("number");
@@ -13,4 +12,16 @@ function iniciarTiempo() {
       }, 1000);
       inicio++;
   }
+}*/
+
+var iniciarTiempo = () => {
+    if (inicio == 0) {
+        let n:number = 1;
+        let l:any = document.getElementById("number");
+        crono = window.setInterval(function () {
+            l.innerHTML = n;
+            n++;
+        }, 1000);
+        inicio++;
+    }
 }
